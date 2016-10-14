b30469843638c8e754b5b35e25d8d1b699ba0afd
Cheok Swin Voon <cheoksv@my.ibm.com>
2016-10-14 17:22:44 +0800

    remove embedded tomcat, deploy as war

--- src/main/java/com/example/VerbApplication.java
+++ src/main/java/com/example/VerbApplication.java
@@ -2,13 +2,21 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class VerbApplication {
+@EnableEurekaClient
+public class VerbApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(VerbApplication.class, args);
 	}
+	
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(VerbApplication.class);
+    }
 }
